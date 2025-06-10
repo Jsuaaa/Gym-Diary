cc34ddd4cfe95ebe68e11a5eb4a7b7a759d415fe
JuanSBr <juanpablosuarezbrango@hotmail.com>
2025-06-10 17:15:47 -0500

    - Card Component Done.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DashboardView from '@/views/Dashboard-View.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: DashboardView,
+    },
+  ],
 })
 
 export default router
---
+++ src/views/Dashboard-View.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import CardComponent from '@/components/Card-Component.vue'
+import { Target } from 'lucide-vue-next'
+import { TrendingUp } from 'lucide-vue-next'
+import { Calendar } from 'lucide-vue-next'
+</script>
+
+<template>
+  <!-- Card Container -->
+  <div class="w-full p-4 md:p-6 bg-blue-100 grid grid-cols-1 md:grid-cols-3 gap-4 items-stretch">
+    <CardComponent
+      class="w-full h-full"
+      title="Days Trained"
+      :days="0"
+      date="This Year"
+      :Icon="Target"
+    />
+    <CardComponent
+      class="w-full h-full"
+      title="Current Streak"
+      :days="0"
+      date="Consecutive Days"
+      :Icon="TrendingUp"
+    />
+    <CardComponent
+      class="w-full h-full"
+      title="Consistency"
+      :days="0"
+      date="Of the Year"
+      :Icon="Calendar"
+    />
+  </div>
+</template>
+
+<style lang=""></style>
